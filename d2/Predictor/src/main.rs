9f10f13531d0e8ced6af99181ec8e91ec87188cf
Red Bird <Red.Bird.AX@gmail.com>
2023-02-02 20:14:52 +0200

    d2-p2

--- d2/Predictor/src/main.rs
+++ d2/Predictor/src/main.rs
@@ -2,15 +2,15 @@ pub mod predictor;
 use predictor::Iterator;
 
 fn main() {
-
     let total_score = match Iterator::new("../dataset.txt") {
         Ok(it) => {
-            let mut total_score = 0;
+            let mut total_score = (0, 0);
             loop {
                 match it.next() {
                     Ok(round) => {
                         if let Some(round) = round {
-                            total_score += round.play();
+                            total_score.0 += round.play_part_1();
+                            total_score.1 += round.play_part_2();
                         } else {
                             break total_score;
                         }
@@ -28,5 +28,5 @@ fn main() {
         },
     };
 
-    println!("predicted total score: {}", total_score);
+    println!("predicted total score for part 1: {}, part 2: {}", total_score.0, total_score.1);
 }
--- d2/Predictor/src/predictor.rs
+++ d2/Predictor/src/predictor.rs
@@ -7,11 +7,10 @@ pub struct Iterator {
 }
 
 #[derive(Copy, Clone, PartialEq)]
-pub enum HandShape {
-    Rock,
-    Paper,
-    Scissors,
-}
+pub enum HandShape { Rock, Paper, Scissors }
+
+#[derive(Copy, Clone)]
+enum RoundResult { Win, Loose, Draw }
 
 #[derive(Copy, Clone)]
 pub struct Round {
@@ -46,17 +45,31 @@ impl Iterator {
             moves = (round.player, round.opponent);
         }
 
-        let player = match parse_hand_shape(moves.0) {
+        let player = match Iterator::parse_hand_shape(moves.0) {
             Ok(shape) => shape,
             Err(msg) => return Err(format!("player shape: {}", msg)),
         };
 
-        let opponent = match parse_hand_shape(moves.1) {
+        let opponent = match Iterator::parse_hand_shape(moves.1) {
             Ok(shape) => shape,
             Err(msg) => return Err(format!("opponent shape: {}", msg)),
         };
 
         Ok(Some(Round{ player, opponent }))
+    }
+
+    fn parse_hand_shape(symbol: c_char) -> Result<HandShape, String> {
+        let symbol = match std::char::from_u32(symbol as u32) {
+        Some(symbol) => symbol,
+            None => return Err(String::from("can`t parse symbol from iterator")),
+        };
+
+        match symbol {
+            'A' | 'X' => Ok(HandShape::Rock),
+            'B' | 'Y' => Ok(HandShape::Paper),
+            'C' | 'Z' => Ok(HandShape::Scissors),
+            _ => Err(format!("invalid move symbol: {}", symbol))
+        }
     }
 }
 
@@ -69,42 +82,72 @@ impl Drop for Iterator {
 }
 
 impl Round {
-    pub fn play(self) -> i32 {
-        self.get_player_score() + self.get_round_score()
-    }
-
-    fn get_player_score(self) -> i32 {
-        match self.player {
+    fn get_move_score(shape: HandShape) -> i32 {
+        match shape {
             HandShape::Rock => 1,
             HandShape::Paper => 2,
             HandShape::Scissors => 3,
         }
     }
 
-    fn get_round_score(self) -> i32 {
+    fn get_score(result: RoundResult) -> i32 {
+        match result {
+            RoundResult::Loose => 0,
+            RoundResult::Win => 6,
+            RoundResult::Draw => 3,
+        }
+    }
+
+    pub fn play_part_1(self) -> i32 {
+        let result = self.get_round_result_part_1();
+        Round::get_move_score(self.player) + Round::get_score(result)
+    }
+
+    fn get_round_result_part_1(self) -> RoundResult {
         if self.player == self.opponent {
-            return 3;
+            return RoundResult::Draw
         }
         match (self.player, self.opponent) {
             (HandShape::Rock, HandShape::Scissors) |
             (HandShape::Paper, HandShape::Rock) |
-            (HandShape::Scissors, HandShape::Paper) => 6,
-            _ => 0,
+            (HandShape::Scissors, HandShape::Paper) => RoundResult::Win,
+            _ => RoundResult::Loose,
         }
     }
-}
 
-fn parse_hand_shape(symbol: c_char) -> Result<HandShape, String> {
-    let symbol = match std::char::from_u32(symbol as u32) {
-        Some(symbol) => symbol,
-        None => return Err(String::from("can`t parse symbol from iterator")),
-    };
+    pub fn play_part_2(self) -> i32 {
+        let result = self.get_round_result_part2();
+        let player = self.get_player_move(result);
 
-    match symbol {
-        'A' | 'X' => Ok(HandShape::Rock),
-        'B' | 'Y' => Ok(HandShape::Paper),
-        'C' | 'Z' => Ok(HandShape::Scissors),
-        _ => Err(format!("invalid move symbol: {}", symbol))
+        Round::get_move_score(player) + Round::get_score(result)
+    }
+
+    fn get_round_result_part2(self) -> RoundResult {
+        match self.player {
+            HandShape::Rock => RoundResult::Loose,
+            HandShape::Paper => RoundResult::Draw,
+            HandShape::Scissors => RoundResult::Win,
+        }
+    }
+
+    fn get_player_move(self, result: RoundResult) -> HandShape {
+        match result {
+            RoundResult::Loose => {
+                match self.opponent {
+                    HandShape::Rock => HandShape::Scissors,
+                    HandShape::Paper => HandShape::Rock,
+                    HandShape::Scissors => HandShape::Paper,
+                }
+            },
+            RoundResult::Win => {
+                match self.opponent {
+                    HandShape::Rock => HandShape::Paper,
+                    HandShape::Paper => HandShape::Scissors,
+                    HandShape::Scissors => HandShape::Rock,
+                }
+            },
+            RoundResult::Draw => self.opponent
+        }
     }
 }
 
